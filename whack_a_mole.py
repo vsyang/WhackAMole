94749fa162551f1ee79224010564fdf6c47b0929
vsyang <yang.vanessa85@gmail.com>
2025-07-31 16:00:19 -0600

    added placeholder images

--- whack_a_mole.py
+++ whack_a_mole.py
@@ -4,3 +4,18 @@ import random
 WIDTH = 800
 HEIGHT = 600
 TITLE = "Whack A Mole"
+
+MOLE = "mole.png"
+BUNNY = "bunny.png"
+MALLET = "mallet.png"
+HOLES = [
+    (150, 150), 
+    (400, 150),
+    (650, 150),
+    (150, 300),
+    (400, 300),
+    (650, 300) 
+]
+
+MOLE_SCALE = 0.5
+
