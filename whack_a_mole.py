e04a14e8eb3a58277a4d2e646987b3b159ed7d15
vsyang <yang.vanessa85@gmail.com>
2025-08-01 16:00:08 -0600

    level up increases speed and moles

--- whack_a_mole.py
+++ whack_a_mole.py
@@ -31,14 +31,12 @@ class Mole(arcade.Sprite):
         self.center_x, self.center_y = position
         self.alpha = 255
         self.is_visible = True
-        self.visible_time = 0.0
 
     def hide(self):
         self.center_x = -100
         self.center_y = -100
         self.alpha = 0
         self.is_visible = False
-        self.visible_time = 0.0
 
 # main class that defines what the user will view once everything is loaded and ready
 class MainGame(arcade.Window):
@@ -52,26 +50,26 @@ class MainGame(arcade.Window):
         self.score = 0
         self.level = 1
 
-        self.waiting_to_spawn = False
-        self.spawn_delay = 2.0
-        self.time_since_click = 0.0
-        self.mole_time = 2.0
+        self.spawn_delay = 1.0
+        self.display_timer = 2.0
+        self.spawn_timer = 0.0
+        self.active_timer = 0.0
+        self.state = "WAITING"
 
-    # Initialize the game. This function will create 6 random Sprite appearances
+    # Initialize the game. This function will create random Sprite appearances
     def setup(self):
         self.mole_list = arcade.SpriteList()
         self.score = 0
         self.level = 1
-        self.spawn_delay = 2.0
-        self.mole_time = 2.0
+        self.spawn_timer = 0.0
+        self.active_timer = 0.0
+        self.state = "WAITING"
 
-        for _ in range(6):
+        for _ in range(12):
             is_real = random.choice([True, False])
             image = MOLE if is_real else BUNNY
             mole = Mole(image, MOLE_SCALE, is_real)
             self.mole_list.append(mole)
-
-        self.spawn_random_mole()
 
     # Function will show user their score and level
     def on_draw(self):
@@ -84,40 +82,56 @@ class MainGame(arcade.Window):
 
     # Function that checks timing
     def on_update(self, delta_time):
-        if self.waiting_to_spawn:
-            self.time_since_click += delta_time
-            if self.time_since_click >= self.spawn_delay:
-                self.waiting_to_spawn = False
-                self.time_since_click = 0.0
-                self.spawn_random_mole()
-                self.check_level()
+        if self.state == "WAITING":
+            self.spawn_timer += delta_time
+            if self.spawn_timer >= self.spawn_delay:
+                self.spawn_timer = 0.0
+                self.spawn_random_moles()
+                self.state = "VISIBLE"
+        elif self.state == "VISIBLE":
+            self.active_timer += delta_time
+            if self.active_timer >= self.display_timer:
+                for mole in self.mole_list:
+                    mole.hide()
+                self.active_timer = 0.0
+                self.state = "WAITING"
+                self.update_level()
 
-    # Function that will check level of player and increase challenge
-    def check_level(self):
-        new_level = self.score // 4 + 1
-        if new_level > self.level:
-            self.level = new_level
-            
-            self.spawn_delay = max(0.5, self.spawn_delay - 0.2)
 
     # function for moles/bunnies to randomly pop out of their holes. If a bunny pops up, a mole will also pop out so user does not lose points being forced to click bunny
-    def spawn_random_mole(self):
+    def spawn_random_moles(self):
         for mole in self.mole_list:
             mole.hide()
+        
+        if self.level >= 15:
+            count = 4
+        elif self.level >= 10:
+            count = 3
+        elif self.level >= 5:
+            count = 2
+        else:
+            count = 1
 
-        first = random.choice(self.mole_list)
-        first_position = random.choice(HOLES)
-        first.pop_out(first_position)
+        hidden = [m for m in self.mole_list if not m.is_visible]
+        positions = HOLES.copy()
+        random.shuffle(hidden)
+        random.shuffle(positions)
 
-        if not first.is_real:
-            moles_hidden = [m for m in self.mole_list if m.is_real and not m.is_visible]
+        for i in range(min(count, len(hidden), len(positions))):
+            hidden[i].pop_out(positions[i])
 
-            if moles_hidden:
-                second = random.choice(moles_hidden)
+        # first = random.choice(self.mole_list)
+        # first_position = random.choice(HOLES)
+        # first.pop_out(first_position)
 
-                new_positions = [np for np in HOLES if np != first_position]
-                second_position = random.choice(new_positions)
-                second.pop_out(second_position)
+        # if not first.is_real:
+
+        #     if moles_hidden:
+        #         second = random.choice(moles_hidden)
+
+        #         new_positions = [np for np in HOLES if np != first_position]
+        #         second_position = random.choice(new_positions)
+        #         second.pop_out(second_position)
 
     # function that will increase score when mole is hit or decrease score if bunny is hit
     def on_mouse_press(self, x, y, button, modifiers):
@@ -128,9 +142,12 @@ class MainGame(arcade.Window):
                 else:
                     self.score -= 1
                 mole.hide()
-                self.waiting_to_spawn = True
-                self.time_since_click = 0.0
-                break
+
+    # Function that will check level of player and increase challenge
+    def update_level(self):
+        self.level = self.score // 5 + 1
+        self.display_timer = max(1.0 , 2.0 - (self.level - 1) * 0.1)
+            
 
 # function to start
 def main():
